9f741fa76937ddd5fb7e34c714872a048afa6d17
ddluxresearch <107070641+ddluxresearch@users.noreply.github.com>
2022-06-28 16:20:01 -0700

    Add files via upload
    Added comment to change file location to reflect location where user has created their dataset

--- domino_train.py
+++ domino_train.py
@@ -32,6 +32,7 @@ def train_with_params(search_params):
 
 if __name__ == '__main__':
     
+    #####****** ----> CHANGE THIS YOUR DATASET LOCATION <---- ********#######
     input_file ="/repos/UXResearchRepo/iris_data.csv"
 
     iris_df = pd.read_csv(input_file, header = 0)
